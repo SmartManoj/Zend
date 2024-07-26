92bd74484b490f01393b35e8ebe42a7d76a796cf
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-07-26 10:46:33 +0530

    tc

--- TMessagesProj/.cxx/RelWithDebInfo/31t61yz2/x86_64/CMakeFiles/3.10.2/CMakeSystem.cmake
+++ TMessagesProj/.cxx/RelWithDebInfo/31t61yz2/x86_64/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Windows-10.0.22621")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.22631")
 set(CMAKE_HOST_SYSTEM_NAME "Windows")
-set(CMAKE_HOST_SYSTEM_VERSION "10.0.22621")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.22631")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
 include("C:/Users/smart/AppData/Local/Android/Sdk/ndk/21.4.7075529/build/cmake/android.toolchain.cmake")
